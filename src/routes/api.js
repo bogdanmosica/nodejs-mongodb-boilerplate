be8b0271b8e471f5770de0711ae86a2ae47968ea
bogdanmosica <bogdan.mosica@gmail.com>
2022-01-24 18:55:42 +0200

    Fix router error

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const examplesRouter = require('./examples/examples.router');
 
 const api = express.Router();
-
-api.use('/examples', examplesRouter);
 
 module.exports = api;
